fff28acec90d543e7e7847d2fdc704d62b24e59b
El assal Aya <assalharras@gmail.com>
2022-06-03 15:10:31 +0100

    ex5 a

--- app/src/main/java/com/example/cc2tdi201/MyDatabase.java
+++ app/src/main/java/com/example/cc2tdi201/MyDatabase.java
@@ -1,31 +1,46 @@
 package com.example.cc2tdi201;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+
 public class MyDatabase extends SQLiteOpenHelper {
-    public static String DB_NAME="« societes.db";
-    public static String TABLE_NAME="Societe";
+    public static String dbname="« societes.db";
+    public static String table="Societe";
     public static String COL1="ID";
     public static String COL2="NBEMPLOYE";
     public static String COL3="NOM";
     public static String COL4="ACTIVITE";
+
     public MyDatabase(Context c){
-        super(c,DB_NAME,null,1);
+        super(c,dbname,null,1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "create table " + TABLE_NAME + "("+COL1+" integer primary key autoincrement,"+COL2+" INTEGER,"+COL3+" TEXT,"+COL4+"TEXT) ";
+        String sql = "create table " + table + "("+COL1+" integer primary key autoincrement,"+COL2+" INTEGER,"+COL3+" TEXT,"+COL4+"TEXT) ";
         sqLiteDatabase.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        String sql= "DROP TABLE " + TABLE_NAME;
+        String sql= "DROP TABLE " + table;
         sqLiteDatabase.execSQL(sql);
-        onCreate(sqLiteDatabase); 
+        onCreate(sqLiteDatabase);
     }
+
+    public static long AddSociete(SQLiteDatabase db, Societe  e){
+        ContentValues s= new ContentValues();
+        s.put(COL2,e.getNbemploye());
+        s.put(COL3,e.getNom());
+        s.put(COL4,e.getActivite());
+        return db.insert(table,null,s);
+    }
+
+
 
 }
