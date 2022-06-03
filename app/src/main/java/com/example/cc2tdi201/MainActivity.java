b10fd3781140670a0ad9d0c561059c7e5290b194
El assal Aya <assalharras@gmail.com>
2022-06-03 15:53:17 +0100

    ex7

--- app/src/main/java/com/example/cc2tdi201/MainActivity.java
+++ app/src/main/java/com/example/cc2tdi201/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cc2tdi201;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void ajouter(View v){
+        Intent i =new Intent(getApplicationContext(),Activity_add.class);
+        startActivity(i);
+    }
+    public void editer(View v){
+        Intent i =new Intent(getApplicationContext(),Activity_edit.class);
+        startActivity(i);
+    }
+    public void lister(View v){
+        Intent i =new Intent(getApplicationContext(),Activity_lister.class);
+        startActivity(i);
+    }
+
 }
