f41dbbfefe7bed9d4196ce55e65dd896e3d5e8b4
El assal Aya <assalharras@gmail.com>
2022-06-03 16:30:27 +0100

    ex 11  c d

--- app/src/main/java/com/example/cc2tdi201/MainActivity.java
+++ app/src/main/java/com/example/cc2tdi201/MainActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
- 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
