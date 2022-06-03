c695d13ed8c896a6ecdc28b7dc4b0a747e0a80f6
El assal Aya <assalharras@gmail.com>
2022-06-03 14:57:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cc2tdi201/Societe.java
@@ -0,0 +1,53 @@
+package com.example.cc2tdi201;
+
+import java.io.Serializable;
+
+public class Societe implements Serializable {
+    public int id;
+    public int nbemploye;
+    public String nom;
+    public String activite;
+
+    public Societe(){}
+
+    public Societe(int id, int nbemploye, String nom, String activite) {
+        this.id = id;
+        this.nbemploye = nbemploye;
+        this.nom = nom;
+        this.activite = activite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNbemploye() {
+        return nbemploye;
+    }
+
+    public void setNbemploye(int nbemploye) {
+        this.nbemploye = nbemploye;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getActivite() {
+        return activite;
+    }
+
+    public void setActivite(String activite) {
+        this.activite = activite;
+    }
+
+
+}
